f8123af23a8bc77322fc9e9651609124aaded4cb
HildaM03 <melendezhilda920@gmail.com>
2025-05-16 17:45:29 -0600

    Se agrego verificaiones en el login y se agrego la pantalla de Canchas

--- lib/Canchaspage.dart
+++ lib/Canchaspage.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
 class CanchasPage extends StatelessWidget {
-  final List<String> canchas = [
-    'SportMania',
-    'Maracana Palenque',
-    'Los Castaños',
-    'Taki Take',
-    'Campisa'
+  final List<Map<String, String>> canchas = [
+    {
+      'nombre': 'SportMania',
+      'imagen':
+          'https://scontent.fsap12-1.fna.fbcdn.net/v/t39.30808-6/489787440_29434847472795932_8830267978180362267_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=ey9XhkOTiZoQ7kNvwGjxiuw&_nc_oc=AdltDweIoO8THuMbvQcDoE6h8YkB_HckUQ6eQyn87QbN5q0CHtD1ecjfS3FFWUjhsxY&_nc_zt=23&_nc_ht=scontent.fsap12-1.fna&_nc_gid=C4mW_5jjMvSudpMEOOHMbQ&oh=00_AfJPLexBWa2k9lfzdJVWQIw9N7MsYTEgmZyiMoIv4Ndf_Q&oe=682D79C5',
+    },
+    {
+      'nombre': 'Maracana Palenque',
+      'imagen':
+          'https://scontent.fsap12-1.fna.fbcdn.net/v/t39.30808-6/305297731_519801253481214_1238331527813895484_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=0ewi9xTwvCAQ7kNvwHsE24m&_nc_oc=Adml5mTy13RALuS5gnkpX4xchyXWmgFvU7jPIRJghuTMjW6CrhqXmzC0ShkRIH1DKbs&_nc_zt=23&_nc_ht=scontent.fsap12-1.fna&_nc_gid=vjyOfBG9vag50J2bAZzyxA&oh=00_AfJVRBnBqiHBODxDOkWVGFEhCc3AP-UaBpRkq-HfBb7vHg&oe=682D7FB8',
+    },
+    {
+      'nombre': 'Los Castaños',
+      'imagen':
+          'https://media.istockphoto.com/id/1997871782/es/foto/soccer-player-kicking-ball-at-goal.jpg?s=612x612&w=0&k=20&c=UUjRBFiY_PD1plhYrulPIloXErXqVZ0vi97YFobOl5Y=',
+    },
+    {
+      'nombre': 'Cancha Rancho Tara',
+      'imagen':
+      'https://media.istockphoto.com/id/1610631888/es/foto/ni%C3%B1o-jugando-al-f%C3%BAtbol-en-el-parque-local.webp?a=1&b=1&s=612x612&w=0&k=20&c=WOL1stc3wtdT2KhxUvat4xd-6XclIed9hU_RBTd1QRA='
+    ,
+    },
+    {
+      'nombre': 'Campisa',
+      'imagen':
+          'https://images.unsplash.com/photo-1579952363873-27f3bade9f55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmFsJUMzJUIzbiUyMGRlJTIwZiVDMyVCQXRib2x8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+      'nombre': 'Complejo Deportivo Emil Martínez',
+      'imagen':
+          'https://images.unsplash.com/photo-1575361204480-aadea25e6e68?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
   ];
 
   @override
@@ -21,32 +47,62 @@ class CanchasPage extends StatelessWidget {
         child: ListView.builder(
           itemCount: canchas.length,
           itemBuilder: (context, index) {
-            return Card(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              elevation: 5,
-              child: ListTile(
-                contentPadding: EdgeInsets.all(16),
-                title: Text(
-                  canchas[index],
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green.shade800,
-                  ),
+            final cancha = canchas[index];
+            return GestureDetector(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('¡Seleccionaste ${cancha['nombre']}!')),
+                );
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                trailing: Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.green.shade600,
+                elevation: 6,
+                margin: EdgeInsets.only(bottom: 20),
+                clipBehavior: Clip.antiAlias,
+                child: Stack(
+                  children: [
+                    // Imagen
+                    Ink.image(
+                      image: NetworkImage(cancha['imagen']!),
+                      height: 200,
+                      fit: BoxFit.cover,
+                      child: InkWell(onTap: () {}),
+                    ),
+                    // Título superpuesto
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Colors.black54, Colors.transparent],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        child: Text(
+                          cancha['nombre']!,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                  blurRadius: 5,
+                                  color: Colors.black,
+                                  offset: Offset(1, 1))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                onTap: () {
-                  // Aquí puedes agregar la lógica para mostrar más detalles de la cancha seleccionada
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('¡Seleccionaste ${canchas[index]}!')),
-                  );
-                },
               ),
             );
           },
